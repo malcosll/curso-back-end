a09f7b7483892849abe1e672deb5a755ebe710d4
malcosll <162439457+malcosll@users.noreply.github.com>
2024-03-11 10:12:00 -0300

    Add files via upload
    enviando calculadora

---
+++ marcos/calculadora.py
@@ -0,0 +1,9 @@
+num1 = int (input ("Digite o primeiro número: "))
+num2 = int (input ("Digite o segundo número: "))
+
+ad = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+
+print (num1," + ", num2, " = ", ad, "\n", num1," - ", num2, " = ", sub, "\n", num1," / ", num2, " = ", div, "\n", num1," x ", num2, " = ", mul)
